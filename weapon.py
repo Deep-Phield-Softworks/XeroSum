41f8270f77d2f2ca271d7c0e6f1956ab85ceb30a
Chris Toups <Ctoups89@gmail.com>
2015-05-16 19:24:07 -0500

    weapon added to
    added basic stubbed out functionality to weapon

--- weapon.py
+++ weapon.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python
 
-class Weapon:
-    def __init__(self):
-        pass
+'''
+Weapon is descended from Item, which is descended from Matter
 
-#comment
+Accepted **kwargs in self.accepted_kwargs:
+    -'damage' => points to an 'effect' that is not yet implemented. Will probably
+                 be a list or dictionary or some other data structure.
+    -'weapon_range' =>  value of the range of the weapon. 1 by default
+'''
 
-#adding comment to test
+
+class Weapon (Item) :
+    def __init__(self, **kwargs):
+        Item.__init__(self, **kwargs)
+        self.accepted_kwargs = {'damage': 'none', 'weapon_range' : 1}
+        for key in self.accepted_kwargs.keys():
+            if key in kwargs.keys():
+                self.__setattr__(key, kwargs[key])
+            else:
+                self.__setattr__(key, self.accepted_kwargs[key])
+
+
+    def attack (self, target) :
+        error_string = "Subclass of weapon must properly implement it's own attack method!"
+        raise NotImplementedError(error_string)
+
+
+# add in Item  a self.kwarg of weight that is a float and a use_sound in single quotes and a sound.mpr3 or whatever
+# from manifest import fx_manifest
+
+    
