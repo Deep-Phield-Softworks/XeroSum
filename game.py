437df381566c0fe9e77f9796d5ff0148ae874c5e
ArikVhedrovix <efedrowisch@yahoo.com>
2015-05-23 02:36:57 -0500

    Some PEP8 to game.py. Not done yet.

--- game.py
+++ game.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
-import pygame, sys, os, random
+import sys
+import os
+import random
 
+
+import pygame
 import transaction
 
 
 from controller import Controller
-from manifests import screen,  screen_size, tracks,  fx_manifest
+from manifests import screen, screen_size, tracks, fx_manifest
 from world import World
 from player import Player
 from aoe import *
@@ -13,70 +17,76 @@ from worldview import WorldView
 from path import Path
 from entity import Entity
 from tile import Tile
-from unboundmethods import find_parent,  TILE_WIDTH, TILE_HEIGHT
+from unboundmethods import find_parent, TILE_WIDTH, TILE_HEIGHT, adjacent
 
 """
 Game Class for Xero Sum
 Game is a high level wrapper that has the following attributes:
--Has clock object
--Initializes all but the original pygame render surface and image/sound manifests
+-Initializes pygame and pygame sound mixer
+-Has convenience references to world.db and screen surface from
+ manifest
+-Has a controller instance to handle event processing(ui mainly atm)
+-Has a pygame clock that measures ms per main game loop
+-Has a variable self.run that is True while program is running
 """
 
+
 class Game:
-    
-    def __init__(self,  world_name = 'Test',  player_start_coordinate_key = '0_0_0'):
-        pygame.init() 
+
+    def __init__(self,  world_name='Test', start_coordinate_key='0_0_0'):
+        pygame.init()
         pygame.mixer.init()
-        self.platform = sys.platform  #Determine operating system
-        self.world = self.world_init(world_name,  player_start_coordinate_key) #Create world object
-        self.db = self.world.db  #Convenience renaming here
+        self.platform = sys.platform  # Determine operating system
+        self.world = self.world_init(world_name,  start_coordinate_key)
+        self.db = self.world.db  # Convenience renaming here
+        self.screen = screen
         self.screen_size = screen_size
-        self.screen = self.render_surface_init()
+        self.render_surface_init()
         self.view = self.world_view_init()
         self.controller = Controller(self)
-        self.clock = pygame.time.Clock()  #Clock object to tick
-        self.tick  = self.db['tick_accumulator']  #Load last clock tick value
-        self.reality_bubble_range = 1  #How far into inactive chunks tick reaches
+        self.clock = pygame.time.Clock()  # Clock object to tick
+        self.tick = self.db['tick_accumulator']  # Load last clock tick value
         self.screen_text_top = []
-        self.screen_text = [] 
-        self.selected = None  #Entities left clicked
-        self.path_target = None  #Path Finding Target
-        self.run = True #Run while true
+        self.screen_text = []
+        self.selected = None  # Entities left clicked
+        self.path_target = None  # Path Finding Target
+        self.run = True  # Run while true
 
     def commit(self):
         transaction.commit()
-    
-    def world_init(self,  name = 'Test',  player_start_coordinate_key = '0_0_0'):
+
+    def world_init(self, name='Test', start_coordinate_key='0_0_0'):
         world = World(name)
-        if world.db['new_game']:  #If 'new_game'...
-            #Determine origin_key to activate chunks...
-            if world.db['player'] == None:  #If no player yet...
-                origin_key = player_start_coordinate_key #Use given/default coordinates for origin_key
-            else:                                              #Else if player exists...
-                origin_key =world.db['player'].coordinate_key  #Use their coordinate_key
-            #Generate a cube shape
-            cubeargs = {'origin': origin_key, 'magnitude': [10,10,0], 'towards_neg_inf': False} 
+        if world.db['new_game']:  # If 'new_game'...
+            # Determine origin_key to activate chunks...
+            if world.db['player'] == None:  # If no player yet...
+                origin_key = start_coordinate_key  # Use default origin_key
+            else:  # Else if player exists...
+                origin_key = world.db['player'].coordinate_key
+            # Generate a cube shape
+            cubeargs = {'origin': origin_key}
+            cubeargs['magnitude'] = [10, 10, 0]
+            cubeargs['towards_neg_inf'] = False
             shape = Cube(**cubeargs)
-            world.db['view_shape'] = shape #Store shape for later use in self.view
-            #Use shape to determine initial actuve chunks
+            world.db['view_shape'] = shape
+            # Use shape to determine initial actuve chunks
             chunks = []
-            #For each coordinate key in shape.area_key_list
+            # For each coordinate key in shape.area_key_list
             for coordinate_key in shape.area_key_list:
                 parent_chunk = find_parent(coordinate_key)
-                if parent_chunk not in chunks: #If not...
-                    chunks.append(parent_chunk)  #Add it...
+                if parent_chunk not in chunks:  # If not...
+                    chunks.append(parent_chunk)  # Add it...
             world.activate_chunk(*chunks)
-            #Generate random base terrain for active chunks
+            # Generate random base terrain for active chunks
             base = {'image_key': 'grass.png'}
-            #feature(image_key, name = None, speed_modifier = 1.0, tall = 0, float_offset = [0.5,0.5], impassible = False, blocksLOS = False)
-            rocks = {'image_key':'rocks.png', 'speed_modifier': 1.25, 'layer': 1.0}
+            rocks = {'image_key': 'rocks.png', 'speed_modifier': 1.25}
             bushes = {'image_key': 'bush.png', 'speed_modifier': 1.50, 'layer': 1.1}
-            trees  = {'image_key': 'tallTree.png', 
+            trees = {'image_key': 'tallTree.png',
                             'tall': 20,
                             'float_offset': [0.5, 0.5],
                             'layer': 1.2,
                             'impassible': True,
-                            'blocksLOS': True }
+                            'blocksLOS': True}
             for key in sorted(world.db['active_chunks'].keys()):
                 print "##Chunk Building...##", key
                 world.chunk_terrain_base_fill(key, **base)
@@ -106,7 +116,6 @@ class Game:
             self.viewpoint_max_size = self.screen_height_in_tiles
         self.px_offset = (self.screen_size[0]/2) - (TILE_WIDTH/2)
         self.py_offset = (self.screen_size[1]/2) - ((self.viewpoint_max_size * TILE_HEIGHT)/2)
-        return screen
     
     def world_view_init(self):
         #World View Object arguements== WorldView(world, shape, screen_size,  px_offset,  py_offset)
@@ -146,6 +155,9 @@ class Game:
     def reality_bubble_check(self, *elements):
         pass
     
+    def pixel_collison(self, rect, ):
+        pass
+
     def main_loop(self):
         if not self.run: 
             self.world.close()
@@ -158,7 +170,7 @@ class Game:
         self.path()
         self.world.tick(self.clock.tick())
         self.view.render()
-        screen.blit(self.view.surface, (0,0))
+        self.screen.blit(self.view.surface, (0,0))
         #self.draw_screen_text() #Draw text onto screen
         pygame.display.flip()    
 
