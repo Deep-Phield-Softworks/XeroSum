b29da1786308d6abd7c5e16802c46536b1f95543
ArikVhedrovix <efedrowisch@yahoo.com>
2015-05-22 22:14:02 -0500

    dmap now passes automated PEP8 tests

--- dmap.py
+++ dmap.py
@@ -6,25 +6,25 @@ from unboundmethods import *
 
 
 '''
-#Djikstra Map objects that can be combined together and can find paths
-#Given:
-#-shape, a shape template object from AoE.py
-#-selected_dict, a dictionary of coordinate Keys as dict keys and values that
-# will be added to the DMAP
-#Return: a Djikstra Map object that:
-#-can be combined with other Djikstra Maps
-#-should be processed once all desired input DMAPs have been combined
-#-can return a list of nodes using find_path(start)
+Djikstra Map objects that can be combined together and can find paths
+Given:
+-shape, a shape template object from AoE.py
+-selected_dict, a dictionary of coordinate Keys as dict keys and values that
+ will be added to the DMAP
+Return: a Djikstra Map object that:
+-can be combined with other Djikstra Maps
+-should be processed once all desired input DMAPs have been combined
+-can return a list of nodes using find_path(start)
 '''
 
 
 class DijkstraMap:
-    def __init__(self, shape, selected_dict, default_max = 10000):
+    def __init__(self, shape, selected_dict, default_max=10000):
         self.shape = shape
         self.selected_dict = selected_dict
         # The coordinate keys are already in shape...
         # Use shape's nD Key list
-        self.coordinates = self.shape.n_dimensional_array 
+        self.coordinates = self.shape.n_dimensional_array
         # Create a dictionary to store the values
         self.keys_flat_list = []
         self.map_vals = dict()
@@ -35,9 +35,8 @@ class DijkstraMap:
                         self.map_vals[self.coordinates[x][y][z]] = default_max
                         self.keys_flat_list.append(self.coordinates[x][y][z])
         for key in self.selected_dict.keys():
-            # PEP8 says has_key is deprecated, use 'in'
-            if self.map_vals.has_key(key):
-                self.map_vals[key] = self.selected_dict[key] 
+            if key in self.map_vals:
+                self.map_vals[key] = self.selected_dict[key]
         # Using custom mods order to make diagonals "float to top" in paths
         self.mods = [(-1, -1, -1), (1, 1, -1), (-1, 1, -1), (1, -1, -1),
                      (0, -1, -1), (-1, 0, -1), (1, 0, -1), (0, 1, -1),
@@ -46,7 +45,7 @@ class DijkstraMap:
                      (0, 0, 0), (-1, -1, 1), (1, 1, 1), (-1, 1, 1),
                      (1, -1, 1), (0, -1, 1), (-1, 0, 1), (1, 0, 1),
                      (0, 1, 1)]
-    
+
     # You should call this ONLY once all desired inputs have been combined into
     # One DMAP. It must be called before using find_path().
     def process_map(self):
@@ -57,25 +56,23 @@ class DijkstraMap:
                 if self.map_vals[key] != None:
                     low = self.lowest_neighbor_value(key)[0]
                     # PEP8 suggests 'if cond is not None'
-                    if low != None:
+                    if low:
                         if (self.map_vals[key] - low) >= 2:
                             self.map_vals[key] = low + 1
                             again = True
-   
-    # Returns the tuple of info on lowest neighbor it can find.        
+
+    # Returns the tuple of info on lowest neighbor it can find.
     def lowest_neighbor_value(self, key):
         low_key = None
-        print "key",  key
+        print "key", key
         XYZ = key_to_XYZ(key)
         for mod in self.mods:
             initial = [(XYZ[0] + mod[0]), (XYZ[1] + mod[1]), (XYZ[2] + mod[2])]
             trial_key = make_key(initial)
             # Use lazy and eval to avoid "no such key" exception here
-            # PEP8 says has_key is deprecated, use 'in'
-            # PEP8 the line below is too long but I can't change it
-            if self.map_vals.has_key(trial_key) and self.map_vals[trial_key] != None:
+            if trial_key in self.map_vals and self.map_vals[trial_key] != None:
                 low = self.map_vals[trial_key]
-                low_key = trial_key 
+                low_key = trial_key
                 break
         for mod in self.mods:
             lookup = [None, None, None]
@@ -83,18 +80,16 @@ class DijkstraMap:
             lookup[1] = XYZ[1] + mod[1]
             lookup[2] = XYZ[2] + mod[2]
             modKey = make_key(lookup)
-            # PEP8 says has_key is deprecated, us 'in'
-            if self.map_vals.has_key(modKey)and self.map_vals[modKey] != None:
+            if modKey in self.map_vals and self.map_vals[modKey] != None:
                 if self.map_vals[modKey] <= low:
                     low = self.map_vals[modKey]
                     low_key = modKey
         return (low, low_key)
-    
+
     def find_path(self, start_key):
         nodes = [start_key]
         next_node = self.lowest_neighbor_value(nodes[-1])[1]
-        # PEP8 suggests 'is not None'
-        while(next_node != None):
+        while(next_node is not None):
             next_node = self.lowest_neighbor_value(nodes[-1])[1]
             if next_node != nodes[-1]:
                 nodes.append(next_node)
@@ -106,15 +101,13 @@ class DijkstraMap:
     # either DMAP it becomes None. Otherwise the values of both are added.
     def combine(self, DMAP):
         for key in self.keys_flat_list:  # For key in self..
-            # PEP8 says has_key is deprecated, use 'in'
-            if DMAP.map_vals.has_key(key):  # If key in DMAP...
+            if key in DMAP.map_vals:  # If key in DMAP...
                 if DMAP.map_vals[key] == None or self.map_vals[key] == None:
                     self.map_vals[key] = None  # Set value
                 else:
-                    # PEP8 this line is one char too long. But I can't change
-                    self.map_vals[key] = self.map_vals[key] + DMAP.map_vals[key]
+                    self.map_vals[key] += DMAP.map_vals[key]
         return self
-    
+
     # Combine two DMAPs. Then expand the calling DMAP by the unique coordinates
     # in the argument DMAP.
     def expand(self, DMAP):
@@ -122,31 +115,30 @@ class DijkstraMap:
         self = self.combine(DMAP)
         # Add new keys from DMAP
         for key in DMAP.keys_flat_list:  # F or key in DMAP
-            # PEP8 says has_key is deprecated, use 'in'
-            if not self.map_vals.has_key(key):  # If key not in self
+            if key not in self.map_vals:  # If key not in self
                 # Add key & value to self
                 self.map_vals[key] = DMAP.map_vals[key]
                 # Add key to flat list
-                self.keys_flat_list.append(key) 
+                self.keys_flat_list.append(key)
         # Lastly make a new expanded DMAP, populating the empty cells w/ None
         self_bounds = self.bounds()  # Find the bounds
         DMAP_bounds = DMAP.bounds()
         # Find the origin
-        origin = make_key(self.lower(self_bounds, DMAP_bounds))  
+        origin = make_key(self.lower(self_bounds, DMAP_bounds))
         magnitude = (abs(self_bounds[0] - DMAP_bounds[0]),
                      abs(self_bounds[1] - DMAP_bounds[1]),
                      abs(self_bounds[2] - DMAP_bounds[2]))
         new_shape = Cube(origin, magnitude, True)  # Create a new Shape
         expanded = DijkstraMap(new_shape, self.map_vals, None)
         return expanded
-    
+
     # Define the bounaries as a pair of lowest (x,y,z) and greatest
     # (x,y,z) values as determiend by using the calling DMAP's self.coordinates
     def bounds(self):
         low = key_to_XYZ(self.coordinates[0][0][0])
         hi = key_to_XYZ(self.coordinates[-1][-1][-1])
         return (low, hi)
-    
+
     # Given two points in form: (x,y,z)
     # Return: Lower of the two points
     def lower(self, a, b):
@@ -158,15 +150,15 @@ class DijkstraMap:
         elif a[2] > b[2]:
             low = b
         return low
-    
+
 if __name__ == '__main__':
     # Run a test
     # Make a key to use in shape object creation
-    origin = [0, 0, 0] 
-    oKey = make_key(origin) 
+    origin = [0, 0, 0]
+    oKey = make_key(origin)
     # Make a Cube shape object
     # Shape will extend from (0,0,0) to (20,20,0) in cube shape
-    shape = Cube(oKey, [21, 21, 1], True)  
+    shape = Cube(oKey, [21, 21, 1], True)
     # Dictionary of selected cells and their values.
     # Value of 0 is a goal, None is impassible
     selected_dict = dict()
@@ -181,4 +173,4 @@ if __name__ == '__main__':
     # Find a path given a start point
     print d.find_path('0_0_0')
     print "Bounds"
-    print d.bounds()    
+    print d.bounds()
