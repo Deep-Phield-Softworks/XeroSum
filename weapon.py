8174d70e8442196e950ceeca908010945b8a28b2
ArikVhedrovix <efedrowisch@yahoo.com>
2015-05-29 15:43:59 -0500

    PEP8 on weapon.py. Minor comment changes and line length changes. Fixed None parameter.

--- weapon.py
+++ weapon.py
@@ -4,25 +4,21 @@
 Weapon is descended from Item, which is descended from Matter
 
 Accepted **kwargs in self.accepted_kwargs:
-    -'damage' => points to an 'effect' that is not yet implemented. Will probably
-                 be a list or dictionary or some other data structure.
+    -'damage' => points to an Effect.
     -'weapon_range' =>  value of the range of the weapon. 1 by default
 '''
 
 
-class Weapon (Item) :
+class Weapon(Item):
     def __init__(self, **kwargs):
         Item.__init__(self, **kwargs)
-        self.accepted_kwargs = {'damage': 'none', 'weapon_range' : 1}
+        self.accepted_kwargs = {'damage': None, 'weapon_range': 1}
         for key in self.accepted_kwargs.keys():
             if key in kwargs.keys():
                 self.__setattr__(key, kwargs[key])
             else:
                 self.__setattr__(key, self.accepted_kwargs[key])
 
-
-    def attack (self, target) :
-        error_string = "Subclass of weapon must properly implement it's own attack method!"
+    def attack(self, target):
+        error_string = "Weapon subclass needs attack method!"
         raise NotImplementedError(error_string)
-
-    
